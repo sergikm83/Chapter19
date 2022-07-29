17b7cbd5b524e450c24150d871fa7c1ffaf99553
Sergey Misko <spy_3462@mail.ru>
2022-07-29 15:05:20 +0400

    в Main() создал экземпляр BinaryOp для метода Add(). Передал 2 значения делегату и вернул результат в answer

--- SyncDelegateReview/Program.cs
+++ SyncDelegateReview/Program.cs
@@ -8,7 +8,17 @@ namespace SyncDelegateReview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Sync Delegate Review *****");
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Main() invoked thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            // Вызвать Add() в синхронном режиме.
+            BinaryOp b = new BinaryOp(Add);
+            // Можно было бы также написать b.Invoke(10, 10);
+            int answer = b(10, 10);
+            // Эти строки кода не выполнятся до тех пор,
+            // пока не завершится метод Add().
+            Console.WriteLine("Doing more work in Main()"); ;
+            Console.WriteLine("10 + 10 is {0}.", answer);
         }
         static int Add(int x, int y)
         {
