f7b91b4d65987bc2fefce6b19a9d3cac741e24a5
Sergey Misko <spy_3462@mail.ru>
2022-07-29 15:00:23 +0400

    добавил статический метод Add() для сложения 2х чисел.

--- SyncDelegateReview/Program.cs
+++ SyncDelegateReview/Program.cs
@@ -10,5 +10,13 @@ namespace SyncDelegateReview
         {
             Console.WriteLine("Hello World!");
         }
+        static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Add() invoked thread {0}.",Thread.CurrentThread.ManagedThreadId);
+            // Сделать паузу для моделирования длительной операции.
+            Thread.Sleep(5000);
+            return x + y;
+        }
     }
 }
