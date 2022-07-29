8335ac2f78c845341f50404036aab3b9afe2b0b5
Sergey Misko <spy_3462@mail.ru>
2022-07-29 15:46:56 +0400

    асинхронно вызвал метод Add() через делегат BinaryOp

--- AsyncDelegate/Program.cs
+++ AsyncDelegate/Program.cs
@@ -8,7 +8,17 @@ namespace AsyncDelegate
         public delegate int BinaryOp(int x, int y);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Async Delegate Invocation *****");
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Main() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            // Вызвать Add() во вторичном потоке.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult ar = b.BeginInvoke(10, 10, null, null);
+            // Выполнить другую работу в первичном потоке...
+            Console.WriteLine("Doing more work in Main()");
+            // По готовности получить результат выполнение метода Add().
+            int answer = b.EndInvoke(ar);
+            Console.WriteLine("10 + 10 is {0}.",answer);
         }
         static int Add(int x, int y)
         {
