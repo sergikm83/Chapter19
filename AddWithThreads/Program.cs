d81142c1dae7876021aee5d06c4000dbf076b0f5
Sergey Misko <spy_3462@mail.ru>
2022-08-01 15:41:39 +0400

    добавил метод Add() в котором проверяется является ли объект AddParams, и если является, то выводится сумма параметров a и b

--- AddWithThreads/Program.cs
+++ AddWithThreads/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace AddWithThreads
 {
@@ -7,6 +8,15 @@ namespace AddWithThreads
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        static void Add(object data)
+        {
+            if (data is AddParams)
+            {
+                Console.WriteLine("ID of thread in Add(): {0}", Thread.CurrentThread.ManagedThreadId);
+                AddParams ap = (AddParams)data;
+                Console.WriteLine("{0} + {1} is {2}", ap.a, ap.b, ap.a + ap.b);
+            }    
         }
     }
 }
