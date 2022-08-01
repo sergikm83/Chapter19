e7e6c24246c6b1fba33f1abb8f07e432f935301f
Sergey Misko <spy_3462@mail.ru>
2022-08-01 16:38:23 +0400

    добавил проект MultiThreadedPrinting

---
+++ MultiThreadedPrinting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiThreadedPrinting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
