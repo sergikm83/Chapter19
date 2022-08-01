9ffaf1d5af529f3d0073d064657822557a7ee5ee
Sergey Misko <spy_3462@mail.ru>
2022-08-01 13:48:19 +0400

    добавил проект ThreadStats

---
+++ ThreadStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
