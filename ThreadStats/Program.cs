20f40fecdaf46a9829be7a61964b17adb482a768
Sergey Misko <spy_3462@mail.ru>
2022-08-01 14:40:04 +0400

    добавил код в Main() и вывел информацию о текущем потоке.

--- ThreadStats/Program.cs
+++ ThreadStats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadStats
 {
@@ -6,7 +7,17 @@ namespace ThreadStats
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Primary Thread stats *****\n");
+            // Получить имя текущего потока.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+            // Имя текущего домена приложения.
+            Console.WriteLine("Name of current AppDomain: {0}",Thread.GetDomain().FriendlyName);
+            // Вывести некоторую статистику о текущем потоке.
+            Console.WriteLine("Thread Name: {0}", primaryThread.Name);
+            Console.WriteLine("Has thread started?: {0}", primaryThread.IsAlive);
+            Console.WriteLine("Priority Level: {0}", primaryThread.Priority);
+            Console.WriteLine("Thread State: {0}", primaryThread.ThreadState);
         }
     }
 }
