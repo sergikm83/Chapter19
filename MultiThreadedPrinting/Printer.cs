7b9102b7b624fed52427a8b280c1eb3db3accb24
Sergey Misko <spy_3462@mail.ru>
2022-08-01 16:42:45 +0400

    добавил класс Printer, добавил блокировку потоков.

---
+++ MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    public class Printer
+    {
+        // Маркер блокировки.
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            // Использовать маркер блокировки.
+            lock (threadLock)
+            {
+                // Вывести информацию о потоке.
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+                // Вывести числа.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(100 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
