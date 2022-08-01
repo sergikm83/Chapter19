fe929393c8059a64851ddab9697dc928d653e824
Sergey Misko <spy_3462@mail.ru>
2022-08-01 15:05:25 +0400

    добавил класс Printer с одним методом PrintNumbers()

---
+++ SimpleMultiThreadApp/Printer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleMultiThreadApp
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            // Вывести информацию о потоке.
+            Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+            // Вывести числа.
+            Console.Write("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}, ", i);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
