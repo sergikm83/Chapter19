1abf955c7ae405ce0adeec2a916bd0174592c137
Sergey Misko <spy_3462@mail.ru>
2022-08-01 15:38:00 +0400

    добавил класс AddParams

---
+++ AddWithThreads/AddParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddWithThreads
+{
+    class AddParams
+    {
+        public int a, b;
+        public AddParams(int numb1, int numb2)
+        {
+            a = numb1;
+            b = numb2;
+        }
+    }
+}
