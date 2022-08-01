0815f85bc057a269dee0c14e102b614ecc03d1c8
Sergey Misko <spy_3462@mail.ru>
2022-08-01 15:36:07 +0400

    добавил проект AddWithThreads

---
+++ AddWithThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddWithThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
